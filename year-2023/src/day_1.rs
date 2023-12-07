00124842e8385d77e10a1c2ac4ba80e33b15b97e
Ocinom <monsjiscurrently@gmail.com>
2023-12-08 00:15:57 +1000

    Wrapped up day 1 using Aho-Corasick

--- year-2023/src/day_1.rs
+++ year-2023/src/day_1.rs
@@ -1,15 +1,5 @@
 pub mod ex_1 {
     use std::fs;
-    use std::env;
-
-    pub fn read_file() -> String {
-        let ex1_path = "1_1.txt";
-
-        let contents = fs::read_to_string(ex1_path)
-            .expect("Should have been able to read the file");
-
-        contents
-    }
 
     pub fn get_digits(input: &str) -> u32 {
         let chars: Vec<char> = input.chars().collect();
@@ -44,7 +34,7 @@ pub mod ex_1 {
     }
 
     pub fn execute(path: &str) -> Result<u32, ()> {
-        let contents = fs::read_to_string(&path)
+        let contents = fs::read_to_string(path)
             .expect("File {path} not found.");
 
         let mut sum: u32 = 0;
@@ -53,6 +43,74 @@ pub mod ex_1 {
             sum += get_digits(line);
         }
 
+        Ok(sum)
+    }
+}
+
+pub mod ex_2 {
+    use std::{fs, fmt::Display};
+    use aho_corasick::AhoCorasick;
+
+    const nums: &[&str; 18] = &["1", "2", "3", "4", "5", "6", "7", "8", "9",
+    "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+
+    pub fn text_to_digit(input: &str) -> u32 {
+        match input {
+            "1" | "one" => 1,
+            "2" | "two" => 2,
+            "3" | "three" => 3,
+            "4" | "four" => 4,
+            "5" | "five" => 5,
+            "6" | "six" => 6,
+            "7" | "seven" => 7,
+            "8" | "eight" => 8,
+            "9" | "nine" => 9,
+            _ => 0
+        }
+    }
+
+    pub fn get_digits(input: &str) -> u32 {
+        let matches = find_overlapping_matches(input);
+        if matches.len() == 1 {
+            let first_slice = matches[0];
+            let digit = text_to_digit(&input[first_slice.0..first_slice.1]);
+            return 11 * digit;
+        } else if matches.len() > 1 {
+            let first_slice = matches[0];
+            let last_slice = matches.last().unwrap();
+
+            let first_digit = text_to_digit(&input[first_slice.0 .. first_slice.1]);
+            let last_digit = text_to_digit(&input[last_slice.0 .. last_slice.1]);
+
+            return first_digit * 10 + last_digit;
+        }
+        0
+    }
+
+
+    pub fn find_overlapping_matches(input: &str) -> Vec<(usize, usize)> {
+        let ac = AhoCorasick::builder()
+            .ascii_case_insensitive(true)
+            .build(nums)
+            .unwrap();
+
+        let mut matches = vec![];
+
+        for mat in ac.find_overlapping_iter(input) {
+            matches.push((mat.start(), mat.end()));
+        }
+        matches
+    }
+
+
+    pub fn execute(path: &str) -> Result<u32, ()> {
+        let contents = fs::read_to_string(path)
+            .expect("File {path} not found.");
+
+        let mut sum: u32 = 0;
+        for line in contents.split("\n") {
+            sum += get_digits(line);
+        }
         Ok(sum)
     }
 }
--- year-2023/src/main.rs
+++ year-2023/src/main.rs
@@ -1,9 +1,11 @@
-use crate::day_1::{ex_1};
+use crate::day_1::{ex_1, ex_2};
 
 mod day_1;
 
 fn main() {
-    let contents = ex_1::execute("1_1.txt").unwrap();
+    let ex1_ans = ex_1::execute("day_1.txt").unwrap();
+    let ex2_ans = ex_2::execute("day_1.txt").unwrap();
 
-    println!("With text:\n{contents}");
+    println!("Exercise 1 Answer:\n{ex1_ans}");
+    println!("Exercise 2 Answer:\n{ex2_ans}");
 }
