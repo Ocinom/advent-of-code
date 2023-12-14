df33355270eedb4d8f850295471e93995e679b23
Ocinom <monsjiscurrently@gmail.com>
2023-12-14 12:45:25 +1000

    Finished part 2 of day 2

--- year-2023/src/day_2.rs
+++ year-2023/src/day_2.rs
@@ -41,7 +41,7 @@ pub mod ex_1 {
         }
     }
 
-    pub fn execute(file: &str) {
+    pub fn execute(file: &str) -> usize {
         let mut total: usize = 0;
 
         for line in read_to_string(file).expect("File not found.").lines() {
@@ -51,6 +51,47 @@ pub mod ex_1 {
             };
         }
 
-        println!("{:?}", total);
+        total
+    }
+}
+
+pub mod ex_2 {
+    use regex::Regex;
+    use once_cell::sync::Lazy;
+    use std::fs::read_to_string;
+    
+    const MAX_RED: usize = 12;
+    const MAX_GREEN: usize  = 13;
+    const MAX_BLUE: usize = 14;
+
+    const ITEMS_TALLY_RE: &str = r"(?:(\d+) (red|green|blue))";
+
+    static ITEMS_TALLY: Lazy<Regex> = Lazy::new(|| Regex::new(ITEMS_TALLY_RE).unwrap());
+
+    pub fn get_cubes_power(input: &str) -> usize {
+        let mut red_min_so_far = 0;
+        let mut green_min_so_far = 0;
+        let mut blue_min_so_far = 0;
+        for (_, [number, color]) in ITEMS_TALLY.captures_iter(input).map(|c| c.extract()) {
+            let num = number.parse::<usize>().unwrap();
+            match color {
+                "red" => red_min_so_far = if num > red_min_so_far {num} else {red_min_so_far},
+                "green" => green_min_so_far = if num > green_min_so_far {num} else {green_min_so_far},
+                "blue" => blue_min_so_far = if num > blue_min_so_far {num} else {blue_min_so_far},
+                _ => todo!(),
+            };
+        }
+
+        red_min_so_far * green_min_so_far * blue_min_so_far
+    }
+
+    pub fn execute(file: &str) -> usize {
+        let mut total: usize = 0;
+
+        for line in read_to_string(file).expect("File not found.").lines() {
+            total += get_cubes_power(line);
+        }
+
+        total
     }
 }
--- year-2023/src/main.rs
+++ year-2023/src/main.rs
@@ -8,7 +8,9 @@ fn main() {
     // bench(d1e1);
     // bench(d1e2);
 
-    day_2::ex_1::execute("day_2.txt");
+    println!("Day 2 benches: ");
+    bench(d2e1);
+    bench(d2e2);
 }
 
 fn bench(test: fn()) {
@@ -20,11 +22,21 @@ fn bench(test: fn()) {
 }
 
 fn d1e1() {
-    let ex1_ans = ex_1::execute("day_1.txt").unwrap();
+    let ex1_ans = day_1::ex_1::execute("day_1.txt").unwrap();
     println!("Exercise 1 Answer:\n{ex1_ans}");
 }
 
 fn d1e2() {
-    let ex2_ans = ex_2::execute("day_1.txt").unwrap();
+    let ex2_ans = day_1::ex_2::execute("day_1.txt").unwrap();
+    println!("Exercise 2 Answer:\n{ex2_ans}");
+}
+
+fn d2e1() {
+    let ex1_ans = day_2::ex_1::execute("day_2.txt");
+    println!("Exercise 1 Answer:\n{ex1_ans}");
+}
+
+fn d2e2() {
+    let ex2_ans = day_2::ex_2::execute("day_2.txt");
     println!("Exercise 2 Answer:\n{ex2_ans}");
 }
