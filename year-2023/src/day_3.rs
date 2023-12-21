75b03eafc3708f0a962638a3c8778c228dbaf0a7
Ocinom <monsjiscurrently@gmail.com>
2023-12-21 19:34:30 +1000

    Day 3 part 2 progress

--- year-2023/src/day_3.rs
+++ year-2023/src/day_3.rs
@@ -1,10 +1,5 @@
 pub mod ex_1 {
-    use regex::Regex;
     use std::fs::read_to_string;
-    use once_cell::sync::Lazy;
-    use std::cmp::{max, min};
-
-    static SYMBOLS: Lazy<Regex> = Lazy::new(|| Regex::new(r#"\d+"#).unwrap());
 
     pub fn is_symbol(ch: char) -> bool{
         return ch != '.' && !ch.is_numeric();
@@ -53,7 +48,7 @@ pub mod ex_1 {
                     // Why
                     // Also, collecting contiguous numerical strings to form its
                     // respective number
-                    let mut num_parsed: usize;
+                    let num_parsed: usize;
                     if let Ok(num) = input[row][left..right+1]
                         .iter()
                         .map(|c| c.to_string())
@@ -66,7 +61,6 @@ pub mod ex_1 {
                         continue;
                     }
 
-                    println!("{}", num_parsed);
 
                     // Annoying boundary checks
                     let left_peek = if left > 0 {left - 1} else {left};
@@ -124,5 +118,92 @@ pub mod ex_1 {
         }
 
         println!("{:?}", total);
+    }
+}
+
+pub mod ex_2 {
+    use std::fs::read_to_string;
+
+    pub fn is_symbol(ch: char) -> bool{
+        return ch != '.' && !ch.is_numeric();
+    }
+
+    pub fn execute(file: &str) {
+        let file_data = read_to_string(file).expect("File not found.");
+        // vec![ vec![ 'a', 'b', 'c'], vec!['d', 'e', 'f'] ... ]
+        let input: Vec<Vec<char>> = file_data.lines()
+            .map(|l| l.chars().collect::<Vec<char>>())
+            .collect();
+
+        let mut total = 0;
+
+        let width = input[0].len();
+        let height = input.len();
+
+        // First, find the symbols
+        for row in 0..height {
+            let mut cur = 0;
+
+            for col in 0..width {
+                if is_symbol(input[row][col]) {
+
+                    let mut nums: Vec<usize> = Vec::new();
+
+                    // Next, check for numbers around the symbols
+
+                    // Check the left side
+                    if col > 0 && input[row][col - 1].is_numeric() {
+                        let mut left = col;
+
+                        // Expand number to the left
+                        while input[row][left].is_numeric() && left > 1 {
+                            left -= 1;
+
+                            if let Ok(num) = input[row][left..col]
+                                .iter()
+                                .map(|c| c.to_string())
+                                .collect::<Vec<String>>()
+                                .join("")
+                                .parse() {
+                                    nums.push(num);
+                                } else {
+                                    println!("{:?}", input[row][left..col].iter().collect::<Vec<&char>>());
+                                }
+                        }
+                    }
+
+                    // Check the right side
+                    if col < input[row].len() - 1 && input[row][col + 1].is_numeric() {
+                        let mut right = col;
+
+                        // Expand number to the right 
+                        while input[row][right].is_numeric() && right < width {
+                            right += 1;
+
+                            if let Ok(num) = input[row][col..right]
+                                .iter()
+                                .map(|c| c.to_string())
+                                .collect::<Vec<String>>()
+                                .join("")
+                                .parse() {
+                                    nums.push(num);
+                                } else {
+                                    println!("{:?}", input[row][col..right].iter().map(|c| c.to_string()).collect::<Vec<String>>());
+                                }
+                        }
+                    }
+
+                    
+
+                    // Check the top
+
+                    // Check the bottom
+
+                    // If there are two numbers, calculate the gear ratio and add to total
+                }
+            }
+        }
+        // Keep track of those numbers
+        // If exactly two numbers, add them
     }
 }
